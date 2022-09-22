95f0622226869ba85f2508bf0a90d78d697beb64
Rajesh Mohan <50743110+rajesh-mohan@users.noreply.github.com>
2022-09-22 20:11:37 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 from os import getenv, environ
 
 print(str(getenv("MY_PASSWORD")))
+if "blablapass" == getenv("MY_PASSWORD"):
+    print("true")
+else:
+    print("false")
 print(environ["MY_USERNAME"])
